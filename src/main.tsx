9b493070c81809fed9b2a35fdb1268f6601b244e
Obinna Anosike <smartlify09@gmail.com>
2025-07-01 22:20:05 +0100

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import HomePage from './pages/Home/home.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HomePage />} path="/" />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
