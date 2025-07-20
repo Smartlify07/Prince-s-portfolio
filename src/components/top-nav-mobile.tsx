aa7cfddea6f86cd59519e312ce258954f3fd9ef3
Obinna Anosike <smartlify09@gmail.com>
2025-07-20 20:30:08 +0100

    fix path

--- src/components/top-nav-mobile.tsx
+++ src/components/top-nav-mobile.tsx
@@ -1,5 +1,5 @@
 import { Calendar, HamburgerMenu, Plain } from '@solar-icons/react';
-import Logo from '@/../public/assets/images/logo.svg';
+import Logo from '@/../public/assets/images/Logo.svg';
 import Wifi from '@/../public/assets/icons/Wifi.svg';
 import Battery from '@/../public/assets/icons/Battery.svg';
 import Cellular from '@/../public/assets/icons/cellular.svg';
