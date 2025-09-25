22315d634c2bca82927278bb7f35ccafadb2afe4
Obinna Anosike <smartlify09@gmail.com>
2025-09-25 20:05:35 +0100

    fix build eror

--- src/lib/projects/tabbio.ts
+++ src/lib/projects/tabbio.ts
@@ -1,5 +1,5 @@
 import type { Project } from '../types';
-import TabbioImage from '@/../public/assets/images/projects/Tabbio.png';
+import TabbioImage from '@/../public/assets/images/projects/tabbio.png';
 import DesignVector from '@/../public/assets/icons/designVector.svg';
 import Figma from '@/../public/assets/icons/Figma.svg';
 import Layers from '@/../public/assets/icons/Layers.svg';
