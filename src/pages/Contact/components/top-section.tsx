8fc7e045fe18bf3453222885663ada8b865762b4
Obinna Anosike <smartlify09@gmail.com>
2025-10-24 08:41:34 +0100

    fix contact page  top image

--- src/pages/Contact/components/top-section.tsx
+++ src/pages/Contact/components/top-section.tsx
@@ -3,20 +3,20 @@ import BackgroundImageMobile from '@/../public/assets/images/contact/topsectioni
 
 export const TopSection = () => {
   return (
-    <section className="w-full flex flex-col relative h-[400px]">
+    <section className="relative w-full h-[400px] flex flex-col overflow-hidden">
       {/* Background Image */}
-      <picture>
+      <picture className="absolute inset-0 z-0">
         <source srcSet={BackgroundImage} media="(min-width:1024px)" />
         <source srcSet={BackgroundImageMobile} media="(max-width:1023px)" />
         <img
           src={BackgroundImage}
-          className="w-full h-full  md:block object-cover"
           alt="projects-presentation"
+          className="w-full h-[370px] object-cover"
         />
       </picture>
 
       {/* Gradient Overlay + Text */}
-      <div className="project-gradient absolute z-10 inset-0 overflow-clip flex flex-col justify-end font-geist py-4 px-5 gap-2">
+      <div className="absolute inset-0 z-10 h-full top-0 left-0 project-gradient border border-red-500 flex flex-col justify-end font-geist py-4 px-5 gap-2">
         <div className="flex items-center">
           <div className="flex flex-col gap-2 sm:max-w-[50%]">
             <h2 className="text-[40px]/[50px] font-medium gradient-text tracking-[-2px] text-start">
